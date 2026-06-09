23a869bb5d84a3c1f89de1ca139d6fc503ba1ec0
KSHITIJ8473 <159143147+KSHITIJ8473@users.noreply.github.com>
2026-06-10 04:57:05 +0530

    Update AnikotoPlugin.kt

--- ExampleProvider/src/main/kotlin/com/example/AnikotoPlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/AnikotoPlugin.kt
@@ -1,1 +1,11 @@
+package com.example
 
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+@CloudstreamPlugin
+class AnikotoPlugin : BasePlugin() {
+    override fun load() {
+        registerMainAPI(AnikotoProvider())
+    }
+}

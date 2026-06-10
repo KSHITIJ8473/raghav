52f7f443f6ff984e79972471b2b0bef03bfb0e35
kshitij yadav <159143147+KSHITIJ8473@users.noreply.github.com>
2026-06-10 09:11:47 +0530

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -6,6 +6,6 @@ cloudstream {
     status = 1
     tvTypes = listOf("Anime", "AnimeMovie", "OVA")
     language = "en"
-    iconUrl = "https://anikoto.cz/AnikotoTheme/assets/images/logo.png"
+    iconUrl = "https://www.google.com/search?client=firefox-b-d&hs=jQi&sca_esv=d059007c5155ebc1&sxsrf=ANbL-n6acH9sJOujqiqZzBASklvBSGnLkg:1781062854871&udm=2&fbs=ADc_l-aN0CWEZBOHjofHoaMMDiKpaEWjvZ2Py1XXV8d8KvlI3o6iwGk6Iv1tRbZIBNIVs-5-bUj3iBl-UxHsANYwOkWWQqZAJJdwuRaSoLHfELMHAQFneUwKM50jpvR3lgPoPKND8akQVgLBVTEo53fw1z5raJTC8s1v3NluXpvgoyxRB5GEiIHiTiOmlY36mL4n1wm75zEksHpxaN-QJIsqxMOKBb23Tg&q=POKEMON&sa=X&ved=2ahUKEwiP5qWW4PuUAxURWXADHe5gJ3wQtKgLegQIEhAB&biw=958&bih=946&dpr=1#sv=CAMSVhoyKhBlLURvdUFtOGxOS3Roc2hNMg5Eb3VBbThsTkt0aHNoTToOcWE0RDV5b0RPNC02eE0gBCocCgZtb3NhaWMSEGUtRG91QW04bE5LdGhzaE0YADABGAcg19XEqQtKCBABGAEgASgB"
 }
   

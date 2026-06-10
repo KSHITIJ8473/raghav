8cd70e48406ac27ffb0cfdbe92200d701f47d691
kshitij yadav <159143147+KSHITIJ8473@users.noreply.github.com>
2026-06-10 17:08:30 +0530

    Configure cloudstream plugin for AniSalt
    Added configuration for cloudstream plugin with details.

--- AniSaltPlugin/build.gradle.kts
+++ AniSaltPlugin/build.gradle.kts
@@ -1,1 +1,15 @@
+version = 8
 
+cloudstream {
+    language = "hi"
+    description = "Anime/Cartoon in Hindi"
+    authors = listOf("laddu100")
+    status = 1
+    tvTypes = listOf(
+        "AnimeMovie",
+        "Anime",
+        "Cartoon"
+    )
+    iconUrl = "https://www.google.com/s2/favicons?domain=animesalt.ac&sz=%size%"
+    isCrossPlatform = true
+}

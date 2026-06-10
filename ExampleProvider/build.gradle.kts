be7c6facb6563bd297fd1471ced36acd2fbc2120
kshitij yadav <159143147+KSHITIJ8473@users.noreply.github.com>
2026-06-10 09:05:29 +0530

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,15 +1,11 @@
 version = 1
 
 cloudstream {
-    language = "en"
-    description = "Anikoto anime provider"
+    description = "Anime from Anikoto.cz — Sub & Dub"
     authors = listOf("KSHITIJ8473")
     status = 1
-    tvTypes = listOf(
-        "Anime",
-        "AnimeMovie",
-        "OVA"
-    )
-    requiresResources = false
-    iconUrl = "https://anikoto.cz/AnikotoTheme/assets/images/favicon.png"
+    tvTypes = listOf("Anime", "AnimeMovie", "OVA")
+    language = "en"
+    iconUrl = "https://anikoto.cz/AnikotoTheme/assets/images/logo.png"
 }
+  

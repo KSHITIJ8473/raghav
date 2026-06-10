69edd73e42b7568f6cbcb4df3b8a8a93d2f34e8e
kshitij yadav <159143147+KSHITIJ8473@users.noreply.github.com>
2026-06-10 08:55:44 +0530

    Refactor AnikotoPlugin to extend BasePlugin

--- ExampleProvider/src/main/kotlin/com/example/AnikotoPlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/AnikotoPlugin.kt
@@ -1,12 +1,11 @@
 package com.example
 
-import android.content.Context
+import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class AnikotoPlugin : Plugin() {
-    override fun load(context: Context) {
+class AnikotoPlugin : BasePlugin() {
+    override fun load() {
         registerMainAPI(AnikotoProvider())
     }
 }

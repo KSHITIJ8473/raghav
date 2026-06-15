d9ef7907910aa380cd66ef94df250792982961b5
kshitij yadav <159143147+KSHITIJ8473@users.noreply.github.com>
2026-06-15 17:08:26 +0530

    Add files via upload

---
+++ NineAnimePlugin/build.gradle.kts
@@ -0,0 +1,15 @@
+version = 1
+
+cloudstream {
+    language = "en"
+    description = "Anime from 9anime.org.lv with Sub and Dub support"
+    authors = listOf("KSHITIJ8473")
+
+    status = 1
+    tvTypes = listOf(
+        "Anime",
+        "AnimeMovie",
+        "OVA"
+    )
+    iconUrl = "https://9anime.org.lv/wp-content/uploads/2024/08/9animecomee-logo.png"
+}

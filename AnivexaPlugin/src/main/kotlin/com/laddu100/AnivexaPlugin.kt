dc0aa55005ba1d768fd886dc3ed984aba80edd36
kshitij yadav <159143147+KSHITIJ8473@users.noreply.github.com>
2026-06-16 12:44:24 +0530

    Update AnivexaPlugin.kt

--- AnivexaPlugin/src/main/kotlin/com/laddu100/AnivexaPlugin.kt
+++ AnivexaPlugin/src/main/kotlin/com/laddu100/AnivexaPlugin.kt
@@ -1,1 +1,11 @@
+package com.laddu100
 
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+@CloudstreamPlugin
+class AnivexaPlugin : BasePlugin() {
+    override fun load() {
+        registerMainAPI(Anivexa())
+    }
+}

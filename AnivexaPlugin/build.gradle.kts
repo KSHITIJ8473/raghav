6bf5117fa4ba680ad0410bb9e74354972a2360f8
kshitij yadav <159143147+KSHITIJ8473@users.noreply.github.com>
2026-06-17 01:25:00 +0530

    Update build.gradle.kts

--- AnivexaPlugin/build.gradle.kts
+++ AnivexaPlugin/build.gradle.kts
@@ -1,1 +1,15 @@
+version = 1
 
+cloudstream {
+    language = "en"
+    description = "Anime from Anivexa with separate Sub and Dub stream configurations"
+    authors = listOf("KSHITIJ8473")
+
+    status = 1
+    tvTypes = listOf(
+        "Anime",
+        "AnimeMovie",
+        "OVA"
+    )
+    iconUrl = "https://anivexa.vercel.app/favicon.ico"
+}

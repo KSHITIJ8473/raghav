72755bcc0539fa090bda430067261d0e6190becf
kshitij yadav <159143147+KSHITIJ8473@users.noreply.github.com>
2026-06-17 20:27:48 +0530

    Update build.gradle.kts

--- AniWavesPlugin/build.gradle.kts
+++ AniWavesPlugin/build.gradle.kts
@@ -2,7 +2,7 @@ version = 1
 
 cloudstream {
     language = "en"
-    description = "not working site added cloudflare 720p sub and dub anime"
+    description = "720p sub and dub anime"
     authors = listOf("KSHITIJ8473")
 
     status = 1

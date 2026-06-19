4c3a79b423bcf8a4fa002b09b2b2acda13cd8a09
kshitij yadav <159143147+KSHITIJ8473@users.noreply.github.com>
2026-06-20 04:25:11 +0530

    Update build.gradle.kts

--- AniDoorPlugin/build.gradle.kts
+++ AniDoorPlugin/build.gradle.kts
@@ -1,15 +1,1 @@
-version = 3
 
-cloudstream {
-    language = "en"
-    description = "DISABLED (UNDER DEVELOPMENT)"
-    authors = listOf("KSHITIJ8473")
-
-    status = 1
-    tvTypes = listOf(
-        "Anime",
-        "AnimeMovie",
-        "OVA"
-    )
-    iconUrl = "https://anidoor.me/assets/img/favicon/favicon.ico"
-}

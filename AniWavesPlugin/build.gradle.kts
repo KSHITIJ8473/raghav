f2b2091e626de906e9687873fb6f14ccdf5d0182
raghav <159143147+KSHITIJ8473@users.noreply.github.com>
2026-06-20 10:22:45 +0530

    Update build.gradle.kts

--- AniWavesPlugin/build.gradle.kts
+++ AniWavesPlugin/build.gradle.kts
@@ -3,7 +3,7 @@ version = 1
 cloudstream {
     language = "en"
     description = "720p sub and dub anime"
-    authors = listOf("KSHITIJ8473")
+    authors = listOf("raghav")
 
     status = 1
     tvTypes = listOf(

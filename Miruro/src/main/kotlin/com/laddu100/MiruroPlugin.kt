f4a370423333f0cbc2755d74a485d00a95feafa1
raghav <159143147+KSHITIJ8473@users.noreply.github.com>
2026-06-21 03:25:57 +0530

    miruro name change

--- Miruro/src/main/kotlin/com/laddu100/MiruroPlugin.kt
+++ Miruro/src/main/kotlin/com/laddu100/MiruroPlugin.kt
@@ -4,7 +4,7 @@ import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
 @CloudstreamPlugin
-class MiruroPlugin : BasePlugin() {
+class Miruro : BasePlugin() {
     override fun load() {
         registerMainAPI(Miruro())
         registerExtractorAPI(MiruroMegaPlay())

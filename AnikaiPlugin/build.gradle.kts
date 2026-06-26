11ddc859b51c49b5d73a4b9f753daa563d3f6e5d
kshitij yadav <159143147+KSHITIJ8473@users.noreply.github.com>
2026-06-26 16:28:54 +0530

    Add Anikai anime provider plugin with custom HLS and script unpacking

---
+++ AnikaiPlugin/build.gradle.kts
@@ -0,0 +1,15 @@
+version = 1
+
+cloudstream {
+    language = "en"
+    description = "Watch anime online for free with Sub and Dub - Anikai Provider"
+    authors = listOf("raghav")
+
+    status = 1
+    tvTypes = listOf(
+        "Anime",
+        "AnimeMovie",
+        "OVA"
+    )
+    iconUrl = "https://www3.anikai.cc/img/logo.png"
+}

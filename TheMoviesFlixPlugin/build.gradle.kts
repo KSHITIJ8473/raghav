6e02327976991ba5ec2a9dda643e19389a96b998
raghav <159143147+KSHITIJ8473@users.noreply.github.com>
2026-06-27 18:35:50 +0530

    Add files via upload

--- TheMoviesFlixPlugin/build.gradle.kts
+++ TheMoviesFlixPlugin/build.gradle.kts
@@ -1,1 +1,14 @@
+version = 1
 
+cloudstream {
+    language = "en"
+    description = "Download movies & TV series in multiple qualities - TheMoviesFlix Provider"
+    authors = listOf("raghav")
+
+    status = 1
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries"
+    )
+    iconUrl = "https://themoviesflix.xyz/wp-content/uploads/2026/06/cropped-favicon-192x192.png"
+}

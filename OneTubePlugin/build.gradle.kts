3ef8a5be2d6a8053833f5b88e4b35c8df7e77200
raghav <159143147+KSHITIJ8473@users.noreply.github.com>
2026-06-27 15:58:19 +0530

    Add files via upload

--- OneTubePlugin/build.gradle.kts
+++ OneTubePlugin/build.gradle.kts
@@ -1,1 +1,14 @@
+version = 1
 
+cloudstream {
+    language = "en"
+    description = "Stream movies & TV shows with multi-server support - OneTube Provider"
+    authors = listOf("raghav")
+
+    status = 1
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries"
+    )
+    iconUrl = "https://www.1tube.org/logo.png"
+}

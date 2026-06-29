223d03afe032b61c0221bb18f734ba38fd2affd0
raghav <159143147+KSHITIJ8473@users.noreply.github.com>
2026-06-29 13:27:29 +0530

    Add files via upload

--- AnimeShrineDownloaderPlugin/build.gradle.kts
+++ AnimeShrineDownloaderPlugin/build.gradle.kts
@@ -1,1 +1,14 @@
+version = 1
 
+cloudstream {
+    language = "en"
+    description = "Download anime in Hindi, English, Tamil, Telugu, Japanese, Malayalam - Multi-Audio Anime"
+    authors = listOf("raghav")
+
+    status = 1
+    tvTypes = listOf(
+        "Anime",
+        "AnimeMovie"
+    )
+    iconUrl = "https://animeshrine.xyz/favicon.ico"
+}

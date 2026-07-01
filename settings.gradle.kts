667f0cdafa777494218252dbec36315662b2c8c1
raghav <159143147+KSHITIJ8473@users.noreply.github.com>
2026-07-02 00:38:22 +0530

    revert back temp changes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name = "CloudstreamPlugins"
 
-val disabled = listOf<String>("LivXowPlugin")
+val disabled = listOf<String>("AnimetsuPlugin", "ExampleProvider", "Anizen","LivXowPlugin", "AniDoorPlugin", "PpvPlugin" )
 
 File(rootDir, ".").eachDir { dir ->
     if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {

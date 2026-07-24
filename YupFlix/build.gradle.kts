4583e0c74c43b9657bf1446fd609790b40332bbe
Raghav <159143147+KSHITIJ8473@users.noreply.github.com>
2026-07-24 08:35:11 +0000

    adding yupflix

---
+++ YupFlix/build.gradle.kts
@@ -0,0 +1,14 @@
+version = 1
+
+cloudstream {
+    language = "hi"
+    description = "YupFlix - Movies & Series in HD"
+    authors = listOf("raghav")
+
+    status = 1
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries"
+    )
+    iconUrl = "https://www.youflix.com/icon0.svg?944696f23099199e"
+}

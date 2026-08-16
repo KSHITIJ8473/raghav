f1aba81d39c2584f115518a0f2e2b49fef84dd25
raghav <159143147+KSHITIJ8473@users.noreply.github.com>
2026-08-16 12:02:55 +0530

    Gpl

--- Multimovies/build.gradle.kts
+++ Multimovies/build.gradle.kts
@@ -3,7 +3,7 @@ version = 1
 cloudstream {
     language = "en"
     description = "Multimovies - Movies, TV Shows & Anime"
-    authors = listOf("raghav")
+    authors = listOf("raghav,phisher")
     status = 1
     tvTypes = listOf("Movie", "TvSeries", "Anime")
     iconUrl = "https://multimovies.motorcycles/wp-content/uploads/2024/01/cropped-CompressJPEG.online_512x512_image.png"

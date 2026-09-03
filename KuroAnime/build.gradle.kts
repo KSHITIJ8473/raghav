1c1eb2e7bdc5e8e58d9afade32bf3f8196c73b42
KSHITIJ8473 <159143147+KSHITIJ8473@users.noreply.github.com>
2026-09-03 10:17:43 +0000

    add kuroanime

---
+++ KuroAnime/build.gradle.kts
@@ -0,0 +1,17 @@
+version = 1
+
+android {
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+cloudstream {
+    description = "KuroAnime - Anime with Sub & Dub"
+    authors = listOf("KSHITIJ8473")
+
+    status = 1
+    tvTypes = listOf("Anime", "AnimeMovie", "OVA")
+    language = "en"
+    iconUrl = "https://kuroanime.lol/icon-192.png"
+}

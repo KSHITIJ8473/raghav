2c09c59387ede4b23c8d9d5adad8b07d041b743a
KSHITIJ8473 <159143147+KSHITIJ8473@users.noreply.github.com>
2026-09-10 17:51:10 +0000

    add re anime

---
+++ ReAnime/build.gradle.kts
@@ -0,0 +1,17 @@
+version = 1
+
+android {
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+cloudstream {
+    description = "Re:ANIME - Anime with Sub & Dub"
+    authors = listOf("KSHITIJ8473")
+
+    status = 1
+    tvTypes = listOf("Anime", "AnimeMovie", "OVA")
+    language = "en"
+    iconUrl = "https://reanime.to/favicon-32x32.png"
+}

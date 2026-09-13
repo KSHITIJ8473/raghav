becdeba0e0ae69d9717d1f8771ab9a72e6ebccf5
raghav <159143147+KSHITIJ8473@users.noreply.github.com>
2026-09-13 13:18:05 +0530

    Update iconUrl to point to anichan.net

--- AniChan/build.gradle.kts
+++ AniChan/build.gradle.kts
@@ -13,5 +13,5 @@ cloudstream {
     status = 1
     tvTypes = listOf("Anime", "AnimeMovie")
     language = "en"
-    iconUrl = "https://www.google.com/s2/favicons?domain=anichan.to&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=anichan.net&sz=%size%"
 }
